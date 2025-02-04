031c153b2f2fb049d436de21a22a2b8e4eaa78db
HDLC <henri.libault-de-la-chevasnerie@fortil.group>
2025-02-04 09:27:15 +0100

    Add command for usercontrol AddRoundButton

--- WPF/Shared/UserControls/AddRoundButton.xaml.cs
+++ WPF/Shared/UserControls/AddRoundButton.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WPF.Shared.UserControls
 {
@@ -8,15 +9,22 @@ namespace WPF.Shared.UserControls
     /// </summary>
     public partial class AddRoundButton : UserControl
     {
+        public static readonly DependencyProperty OptionsCommandProperty =
+            DependencyProperty.Register(
+                "OptionsCommand",
+                typeof(ICommand),
+                typeof(AddRoundButton),
+                new PropertyMetadata(null));
+
+        public ICommand OptionsCommand
+        {
+            get { return (ICommand)GetValue(OptionsCommandProperty); }
+            set { SetValue(OptionsCommandProperty, value); }
+        }
+
         public AddRoundButton()
         {
             InitializeComponent();
-        }
-
-        public event RoutedEventHandler Click
-        {
-            add { AddHandler(Button.ClickEvent, value); }
-            remove { RemoveHandler(Button.ClickEvent, value); }
         }
     }
 }
